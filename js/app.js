1c7e4fbb7b082a21a59cb567999fbf8bc7df05a0
Stu Bolderson <stu.bolderson@aol.com>
2020-05-25 16:45:43 +0200

    Beer ID functionality
    Specific beer id functionality working - ready to add innerHTML data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // UI VARS
 displayContainer = document.getElementById('display-container')
+recipeIdContainer = document.getElementById('recipeID-container')
 form = document.getElementById('search-form')
 message = document.getElementById('confirmation-message')
 searchInput = document.getElementById('search-input')
@@ -7,6 +8,8 @@ let searchValue
 let currentSearch
 
 // EVENT LST
+
+// passes user input to search paramater for beer search
 searchInput.addEventListener('input', updateInput)
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -16,6 +19,22 @@ form.addEventListener('submit', (e) => {
 function updateInput(e) {
     searchValue = e.target.value;
 }
+
+// returns the path data from specific beer ID when clicking the recipe-btn
+displayContainer.addEventListener('click', (e) => {
+    const beerInfo = e.path.find(item => {
+        if (item.classList) {
+            return item.classList.contains('recipe-btn')
+        } else {
+            return false
+        }
+    });
+    console.log(beerInfo);
+    if (beerInfo) {
+        const beerID = beerInfo.getAttribute('data-recipeid');
+        getBeerById(beerID);
+    }
+});
 
 // FUNCTIONS
 
@@ -35,6 +54,7 @@ async function fetchApi(url) {
     return data;
 }
 
+// HTML data for onload beers and searched beers
 function displayData(data) {
     data.forEach(beer => {
         // console.log(beer);
@@ -54,19 +74,6 @@ function displayData(data) {
 // Auto display beer selection for URL API
 async function getBeers() {
     const data = await fetchApi('https://api.punkapi.com/v2/beers')
-    // data.forEach(beer => {
-    //     // console.log(beer);
-    //     const beers = document.createElement('div')
-    //     beers.classList.add('beers')
-    //     beers.setAttribute('data-recipeID', `${beer.id}`)
-    //     beers.innerHTML = `
-    //     <h2>${beer.name.substring(0, 20)}</h2>
-    //     ${beer.image_url ? `<img src="${beer.image_url}"/>` : '<img src="./img/brewdog_logo.jpg" />'}
-    //     <p class="primary-text">${beer.abv} %</p>
-    //     <p class="number-text">${beer.id}</p>
-    //     <button class="recipe-btn" id="recipe-btn">Get Recipe</button>`
-    //     beerContainer.appendChild(beers)
-    // })
     displayData(data)
 }
 
@@ -74,20 +81,6 @@ async function getBeers() {
 async function searchBeer(search) {
     clear()
     const data = await fetchApi(`https://api.punkapi.com/v2/beers?beer_name=${search}`)
-    // data.forEach(beer => {
-    //     // console.log(beer);
-    //     const beers = document.createElement('div')
-    //     beers.classList.add('beers')
-    //     beers.setAttribute('data-recipeID', `${beer.id}`)
-    //     beers.innerHTML = `
-    //     <h2>${beer.name.substring(0, 20)}</h2>
-    //     ${beer.image_url ? `<img src="${beer.image_url}"/>` : '<img src="./img/brewdog_logo.jpg" />'}
-    //     <p class="primary-text">${beer.abv} %</p>
-    //     <p class="number-text">${beer.id}</p>
-    //     <button class="recipe-btn" id="recipe-btn">Get Recipe</button>`
-    //     beerContainer.appendChild(beers)
-    // })
-
     // return message to user to confirm succesful search or not 
     const count = data.length
     if (data.length > 0) {
@@ -96,6 +89,25 @@ async function searchBeer(search) {
     } else {
         message.innerHTML = `{${count}} matches found for : "${searchValue}"`
     }
+}
+
+// returns beer from specific beer ID 
+async function getBeerById(beerID) {
+    const data = await fetchApi(`https://api.punkapi.com/v2/beers?ids=${beerID}`)
+    const beer = data[0]
+    addBeerToDOM(beer)
+}
+
+
+// returns all data from specific beer ID
+function addBeerToDOM(beer) {
+    const beerRecipe = document.createElement('div')
+    beerRecipe.classList.add('beer-recipe')
+    beerRecipe.innerHTML = `hello`
+    recipeIdContainer.appendChild(beerRecipe)
+    displayContainer.style.display = 'none';
+    form.style.display = 'none'
+    console.log(beer)
 }
 
 getBeers()
