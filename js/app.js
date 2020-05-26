cb4a8f11aada282de2f8663eed9c3492ae0e838f
Stu Bolderson <stu.bolderson@aol.com>
2020-05-26 12:44:52 +0200

    DOM elements added succesfully
    All DOM elements are linking to site correctly - user can check recipe, delete - return to home - not styled

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // UI VARS
-displayContainer = document.getElementById('display-container')
-recipeIdContainer = document.getElementById('recipeID-container')
-form = document.getElementById('search-form')
-message = document.getElementById('confirmation-message')
-searchInput = document.getElementById('search-input')
+const displayContainer = document.getElementById('display-container')
+const recipeIdContainer = document.getElementById('recipeID-container')
+const form = document.getElementById('search-form')
+const message = document.getElementById('confirmation-message')
+const searchInput = document.getElementById('search-input')
+const beerRecipe = document.getElementById('beer-recipe')
 let searchValue
 let currentSearch
 
@@ -101,45 +102,101 @@ async function getBeerById(beerID) {
 
 // returns all data from specific beer ID
 function addBeerToDOM(beer) {
+    scrollTo(0, 0)
     console.log(beer)
-    // remove display and form containers
-    displayContainer.style.display = 'none';
+    // set display of containers 
+    recipeIdContainer.style.display = 'block'
+    displayContainer.style.display = 'none'
     form.style.display = 'none'
-    // set array methods
-    const hops = []
-
-    for (let i = 0; i < 100; i++) {
-        if (beer[`ingredients.hops${i}`]) {
-            hops.push(`${beer[`ingredients.hops.name${i}`]} - ${beer[`ingredients.hops.amount.value${i}`]} ${beer[`ingredients.hops.amount.unit${i}`]} (add : ${beer[`ingredients.hops.add${i}`]} | attribute:  ${beer[`ingredients.hops.attribute${i}`]})`);
-        } else {
-            break
-        }
-    }
-    // generate new div
-    const beerRecipe = document.createElement('div')
-    beerRecipe.classList.add('beer-recipe')
-
-
-
+    // set UI vars for METHOD DIV
+    const fermentation = beer.method.fermentation.temp
+    const mash = beer.method.mash_temp[0]
+    // recipe innerHTML DOM elements
     beerRecipe.innerHTML = `
-    <h2>${beer.name}</h2>
-    <button class="delete-btn">Delete</button>
-    <ul>
-        ${hops.map(hop => `<li>${hop}`).join('')}
-    </ul>
-    `
+        <button class"delete-btn">Delete</button>
+        <div class="recipe-info">
+            <div class="recipe-image">
+                ${beer.image_url ? `<img src="${beer.image_url}" />` : '<img src="./img/brewdog_logo.jpg" />'}
+            </div>
+            <div class="recipe-content"> 
+                <div class="recipe-details">   
+                    <h2 class="h2-heading">${ beer.name}</h2> <span>ABV : ${beer.abv}</span>
+                        <p class="recipe-text">Brewers Tips : ${beer.brewers_tips}</p>
+                        <p class="recipe-text">Description : ${beer.description}</p>
+                </div>
+                <div class="recipe-method">
+                    <h3 class="h3-heading">Fermentation</h3>
+                        <p>${fermentation.value} ${fermentation.unit}</p>
+                    <h3 class="h3-heading">Mash</h3>
+                        <p>${mash.temp.value} ${mash.temp.unit} @ ${mash.duration} mins</p>
+                    <div class="method">
+                        <h3 class="h3-heading">Method</h3>
+                            <p class="recipe-text">Boil Volume : ${beer.boil_volume.value} ${beer.boil_volume.unit}</p>
+                            <p class="recipe-text">EBC : ${beer.ebc}</p>
+                            <p class="recipe-text">IBU : ${beer.ibu}</p>
+                            <p class="recipe-text">pH : ${beer.ph}</p>
+                            <p class="recipe-text">SRM : ${beer.srm}</p>
+                            <p class="recipe-text">Target FG : ${beer.target_fg}</p>
+                            <p class="recipe-text">Target OG : ${beer.target_og}</p>
+                            <p class="recipe-text">Volume : ${beer.volume.value} ${beer.volume.unit}</p>
+                    </div>
+                    <div class="food-pairing">
+                        <h3 class="h3-heading">Food Pairing</h3>
+                            <p class="recipe-text">${beer.food_pairing[0]}</p>
+                            <p class="recipe-text">${beer.food_pairing[1]}</p>
+                            <p class="recipe-text">${beer.food_pairing[2]}</p>
+                    </div>
+                </div>
 
+            </div>        
+        </div    
+            `
+    // create divs for the ingredients forEach lists
+    // HOPS
+    const hopsDiv = document.createElement('div')
+    hopsDiv.classList.add('hops-div')
+    hopsDiv.innerHTML = '<h3 class="h3-heading">Hops</h3>'
+    // MALTS
+    const maltsDiv = document.createElement('div')
+    maltsDiv.classList.add('malts-div')
+    maltsDiv.innerHTML = '<h3 class="h3-heading">Malts</h3>'
 
+    // add hops ingredients to DOM
+    const hops = beer.ingredients.hops
+    hops.forEach(hop => {
+        const list = document.createElement('li')
+        list.classList.add('recipe-list')
+        list.innerHTML = `${hop.name} - ${hop.amount.value} ${hop.amount.unit} (add: ${hop.add} | attribute: ${hop.attribute})`
+        hopsDiv.appendChild(list)
+    })
+    // add malt ingredients to DOM
+    const malts = beer.ingredients.malt
+    malts.forEach(malt => {
+        const list = document.createElement('li')
+        list.classList.add('recipe-list')
+        list.innerHTML = `${malt.name} - ${malt.amount.value} ${malt.amount.unit} `
+        maltsDiv.appendChild(list)
+    })
+    // add yeast ingredient to list array and display to DOM
+    const yeastDiv = document.createElement('div')
+    yeastDiv.classList.add('yeast-div')
+    yeastDiv.innerHTML = `
+     <h3 class="h3-heading">Yeast</h3>
+        <p class="recipe-text">${beer.ingredients.yeast}</p>`
 
-
+    // append ing divs to beerRecipe div
+    beerRecipe.appendChild(hopsDiv)
+    beerRecipe.appendChild(maltsDiv)
+    beerRecipe.appendChild(yeastDiv)
+    // Delete BTN evt lst - remove recipe and reload start page
     recipeIdContainer.addEventListener('click', (e) => {
         if (e.target.value = 'delete-btn') {
-            beerRecipe.style.display = 'none'
-            displayContainer.style.display = 'grid';
+            recipeIdContainer.style.display = 'none'
+            displayContainer.style.display = 'grid'
             form.style.display = 'block'
         }
     })
-    recipeIdContainer.appendChild(beerRecipe)
+    // recipeIdContainer.appendChild(beerRecipe)
 
 
 }
